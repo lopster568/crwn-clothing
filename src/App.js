d3c9aca7cf1907ba3d681a35d334fd75c0179513
aarav568 <cool.aarav568@gmail.com>
2021-09-22 22:50:26 +0530

    Lalala

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import react from 'react'
 import './App.css';
 import {Route, Switch} from 'react-router-dom'
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
@@ -18,9 +18,22 @@ class App extends react.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-      this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-        this.setState({currentUser: user})
-    })
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      }
+
+      setCurrentUser(userAuth);
+    });
   }
 
   componentWillUnmount() {
